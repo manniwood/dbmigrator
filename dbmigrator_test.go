09346ea60cc1fdef06fe603e9d2979556b9721cf
Manni Wood <manni.lee.wood@gmail.com>
2020-06-25 21:44:08 -0400

    Turns test into table test.

--- dbmigrator_test.go
+++ dbmigrator_test.go
@@ -20,96 +20,77 @@ func TestMigrate(t *testing.T) {
 		t.Errorf("Error getting item: %v", err)
 	}
 
-	// The following migrations all build on each other.
+	// The following migrations all build on each other
+	// and have to be executed in the order shown; do not
+	// run in parallel.
 
-	// ---- test_migrations/empty
-	migrator, err := New(conn, "test_migrations/empty")
-	if err != nil {
-		t.Errorf("Unable to set up migrator: %v", err)
-	}
-	// Listen to logs from the migrator.
-	var buf bytes.Buffer
-	err = migrator.Migrate(context.Background(), &buf)
-	if err != nil {
-		t.Errorf("Unable to migrate: %v", err)
-	}
-	if !strings.Contains(buf.String(), "No migrations yet") {
-		t.Errorf("Expected no migrations message; got >>>%s<<< instead.", buf.String())
-	}
-
-	// ---- test_migrations/1
-
-	migrator, err = New(conn, "test_migrations/1")
-	if err != nil {
-		t.Errorf("Unable to set up migrator: %v", err)
-	}
-	// Listen to logs from the migrator.
-	buf = bytes.Buffer{}
-	err = migrator.Migrate(context.Background(), &buf)
-	if err != nil {
-		t.Errorf("Unable to migrate: %v", err)
-	}
-
-	var ct int
-	err = conn.QueryRow(context.Background(), "select count(*) from migrations").Scan(&ct)
-	if err != nil {
-		t.Errorf("Could not get migrations row count: %v", err)
-	}
-	if ct != 1 {
-		t.Errorf("Expected 1 migration in migrations table, found %v instead.", ct)
+	tests := []struct {
+		name           string
+		dir            string
+		wantLog        string
+		wantTables     []string
+		wantMigrations []string
+	}{
+		{
+			name:           "Empty migrations dir.",
+			dir:            "test_migrations/empty",
+			wantLog:        "No migrations yet",
+			wantTables:     []string{},
+			wantMigrations: []string{},
+		},
+		{
+			name:           "Migrations dir with a.sql.",
+			dir:            "test_migrations/1",
+			wantLog:        "Did 1 migrations",
+			wantTables:     []string{"a"},
+			wantMigrations: []string{"a.sql"},
+		},
+		{
+			name:           "Migrations dir with a.sql, b.sql, c.sql; a.sql already applied.",
+			dir:            "test_migrations/2",
+			wantLog:        "Did 2 migrations",
+			wantTables:     []string{"b", "c"},
+			wantMigrations: []string{"b.sql", "c.sql"},
+		},
 	}
 
-	err = conn.QueryRow(context.Background(), "select count(*) from migrations where migration = 'a.sql'").Scan(&ct)
-	if err != nil {
-		t.Errorf("Could not get migrations row count for a.sql: %v", err)
-	}
-	if ct != 1 {
-		t.Errorf("a.sql was not found in the migrations table.")
-	}
+	for _, tt := range tests {
+		migrator, err := New(conn, tt.dir)
+		if err != nil {
+			t.Errorf("Unable to set up migrator: %v", err)
+		}
+		// Listen to logs from the migrator.
+		buf := bytes.Buffer{}
+		err = migrator.Migrate(context.Background(), &buf)
+		if err != nil {
+			t.Errorf("Unable to migrate: %v", err)
+		}
+		if !strings.Contains(buf.String(), tt.wantLog) {
+			t.Errorf("Logs expected >>>%s<<<; got >>>%s<<< instead.", tt.wantLog, buf.String())
+		}
 
-	err = conn.QueryRow(context.Background(), "select count(*) from pg_class where relname = 'a'").Scan(&ct)
-	if err != nil {
-		t.Errorf("Problem looking for table a in pg_class: %v", err)
-	}
-	if ct != 1 {
-		t.Errorf("Could not find table a after migrating a.sql.")
-	}
+		// Be sure the migrations made it to the migrations table.
+		for _, migration := range tt.wantMigrations {
+			var ct int
+			err = conn.QueryRow(context.Background(), "select count(*) from migrations where migration = $1", migration).Scan(&ct)
+			if err != nil {
+				t.Errorf("Could not get migrations row count for %s: %v", migration, err)
+			}
+			if ct != 1 {
+				t.Errorf("%s was not found in the migrations table.", migration)
+			}
+		}
 
-	// ---- test_migrations/2
-
-	migrator, err = New(conn, "test_migrations/2")
-	if err != nil {
-		t.Errorf("Unable to set up migrator: %v", err)
-	}
-	// Listen to logs from the migrator.
-	buf = bytes.Buffer{}
-	err = migrator.Migrate(context.Background(), &buf)
-	if err != nil {
-		t.Errorf("Unable to migrate: %v", err)
-	}
-
-	ct = 0
-	err = conn.QueryRow(context.Background(), "select count(*) from migrations").Scan(&ct)
-	if err != nil {
-		t.Errorf("Could not get migrations row count: %v", err)
-	}
-	if ct != 3 {
-		t.Errorf("Expected 3 migration in migrations table, found %v instead.", ct)
-	}
-
-	err = conn.QueryRow(context.Background(), "select count(*) from migrations where migration in ('b.sql', 'c.sql')").Scan(&ct)
-	if err != nil {
-		t.Errorf("Could not get migrations row count for b.sql and c.sql: %v", err)
-	}
-	if ct != 2 {
-		t.Errorf("b.sql and/or c.sql were not found in the migrations table.")
-	}
-
-	err = conn.QueryRow(context.Background(), "select count(*) from pg_class where relname in ('b', 'c')").Scan(&ct)
-	if err != nil {
-		t.Errorf("Problem looking for tables b and c in pg_class: %v", err)
-	}
-	if ct != 2 {
-		t.Errorf("Could not find tables b and/or c after migrating b.sql and c.sql.")
+		// Be sure the tables actually exist in PostgreSQL.
+		for _, table := range tt.wantTables {
+			var ct int
+			err = conn.QueryRow(context.Background(), "select count(*) from pg_class where relname = $1", table).Scan(&ct)
+			if err != nil {
+				t.Errorf("Problem looking for table %s in pg_class: %v", table, err)
+			}
+			if ct != 1 {
+				t.Errorf("Could not find table %s after migration.", table)
+			}
+		}
 	}
 }
